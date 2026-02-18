51edd219287bad095460daa5b95aac4a8d5dc50b
M K V N S MANIKANTA <24pa1a4573@vishnu.edu.in>
2026-02-18 22:17:49 +0530

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,26 @@
+public class Task implements Comparable<Task> {
+
+    private String title;
+    private int priority;
+    private String deadline;
+
+    public Task(String title, int priority, String deadline) {
+        this.title = title;
+        this.priority = priority;
+        this.deadline = deadline;
+    }
+
+    public String getTitle() { return title; }
+    public int getPriority() { return priority; }
+    public String getDeadline() { return deadline; }
+
+    @Override
+    public int compareTo(Task other) {
+        return Integer.compare(this.priority, other.priority);
+    }
+
+    @Override
+    public String toString() {
+        return title + " | Priority: " + priority + " | Deadline: " + deadline;
+    }
+}
